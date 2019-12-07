f2a847e48ef03321dbfe68eeefb7eabc67b54f10
Mitchell Robles <mitchellr92@live.com>
2019-12-07 07:51:51 -0800

    get at and remove at index methods

--- index.js
+++ index.js
@@ -74,8 +74,46 @@ class LinkedList {
   }
 
   // Get at index
+  getAt(index) {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      if (count == index) {
+        console.log(current.data);
+      }
+      count++;
+      current = current.next;
+    }
+
+    return null;
+  }
 
   // Remove at index
+  removeAt(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    let current = this.head;
+    let previous;
+    let count = 0;
+
+    // Remove first
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+    }
+
+    this.size--;
+  }
 
   // Clear list
 
@@ -99,3 +137,10 @@ ll.insertLast(400);
 ll.insertAt(500, 2);
 
 ll.printListData();
+
+console.log("------------")
+
+ll.removeAt(10);
+
+ll.printListData();
+// ll.getAt(10)
